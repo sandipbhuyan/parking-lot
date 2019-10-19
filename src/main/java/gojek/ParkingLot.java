3de2ef6d7a5e2d34acdd58744a0eee8f26636dc5
sandipbhuyan <sandipbhuyan@gmail.com>
2019-10-19 22:42:16 +0530

    (Gradle-init):Intializing the project

---
+++ src/main/java/gojek/ParkingLot.java
@@ -0,0 +1,11 @@
+package gojek;
+
+import org.joda.time.LocalTime;
+
+public class ParkingLot {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    System.out.println("Hello");
+  }
+}
