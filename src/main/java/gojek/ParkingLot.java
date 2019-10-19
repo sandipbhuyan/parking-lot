b8ab97324da1a7c8de9eb220574672d0a920dcf4
sandipbhuyan <sandipbhuyan@gmail.com>
2019-10-20 00:43:08 +0530

    Intialize the main method

--- src/main/java/gojek/ParkingLot.java
+++ src/main/java/gojek/ParkingLot.java
@@ -1,11 +1,36 @@
 package gojek;
 
-import org.joda.time.LocalTime;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ParkingLot {
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    System.out.println("Hello");
+  public static void main(String[] args) throws IOException  {
+    // Infinite Loop until a required solution is not there
+    while(true)
+    {
+      BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
+      String name = reader.readLine();
+      boolean a = false;
+      switch (name.toLowerCase())
+      {
+        case "a":
+          System.out.println("switch");
+          continue;
+        case "b":
+          System.out.println("Hub");
+          continue;
+        case "exit":
+          a = true;
+          break;
+        default:
+          break;
+      }
+      if(a)
+      {
+        break;
+      }
+    }
+
   }
 }
